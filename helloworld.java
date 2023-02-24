ea114aaaccb2358632dcf4ca2fd2ef31a74127e4
Okarian <33908523+MianNaseem@users.noreply.github.com>
2023-02-23 21:57:40 -0500

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - Feb 19th - 10:40 AM - triggered by cron schedulling");
+    System.out.println("Hello World - Feb 19th - 10:50 AM - triggered by webhook");
     }
 }
